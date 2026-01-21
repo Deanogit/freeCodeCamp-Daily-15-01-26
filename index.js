773ec4465b4ccfe34fd5934080a78342c61a7452
Dean Howe <hideanhowe@gmail.com>
2026-01-21 14:45:16 +0000

    .reverse solution

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 // Given an array with two values, return an array with the values swapped.
 
 // For example, given ["A", "B"] return ["B", "A"].
+
+function arraySwap(arr) {
+  console.log(arr);
+  return arr.reverse();
+}
